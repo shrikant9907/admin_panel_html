5b1648166b01eb821f534882e7ead0d606e12fdd
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-13 16:54:42 +0530

    initial setup

--- js/custom.js
+++ js/custom.js
@@ -1,180 +1,168 @@
-jQuery('document').ready(function(){
+ 
+jQuery(document).ready(function(){
+    
+    jQuery('.signup_btn_event').click(function(e){
+        jQuery('#SignupModal').modal('toggle');  
+    }); 
 
-	// Star Rating https://codepen.io/shrikant9907/pen/oKLdmB
-	// Default selection 
-	selected_stars();
-	var starIcon = jQuery('.star_rating .far');
-	// On hover
-	starIcon.hover(function(){
-		jQuery(this).addClass('fas').prevAll().addClass('fas').removeClass('far');
-		jQuery(this).nextAll().addClass('far').removeClass('fas');
-	}, function(){
-		selected_stars();
-	});
-
-	// OnClick
-	starIcon.click(function(){
-		jQuery(this).addClass('selected').siblings().removeClass('selected');
-		jQuery('.rate_success').remove();
-		jQuery(this).parent().after('<div class="text-success rate_success">Thank you for your rating..</div>');
-	});
-
-	// Selected
-	function selected_stars() {
-		jQuery('i.selected').addClass('fas').prevAll().addClass('fas').removeClass('far');
-	}
-
-
-	// Hamburger 
-	jQuery('.hamburger').click(function(e){
-        e.preventDefault(); 
-        jQuery(this).toggleClass('active');
-        jQuery('.sidebar').fadeToggle();   
-        jQuery('.sidebar').toggleClass('active');   
-        jQuery('body').toggleClass('noscroll');   
-        jQuery('body').toggleClass('sidebaractive');   
-	});
-	
-	// Active Sidebar
-	jQuery('.sidebar.active').click(function(e){
-		e.preventDefault();
-	
-		if (e.target !== this)
-    		return;
-	
-		jQuery('.hamburger').click();
-	});
-
-	// Load HTML 
-	jQuery('.actionloadhtml li a').click(function(){
-		var pid = jQuery(this).attr('href').replace('#', '');
-	 	jQuery( "#loadresult" ).attr('src', pid);
-	});
-
-	jQuery('.testimonials_slick').slick();
-
-	jQuery('.hamburger_sign').on('click',function(){
-		jQuery('.hover_menus').toggle();
-	});
-
-	jQuery('.hover_menus').hover(function(){
-		jQuery(this).show();
-	}, function(){
-		jQuery(this).hide();
-	});  
-
-	// Home Carousel
-	jQuery('.home_carousel').slick({
-	  centerMode: false,
-	  centerPadding: '0',
-	  slidesToShow: 1,
-	  responsive: [
-	    {
-	      breakpoint: 768, 
-	      settings: {
-	        arrows: false,
-	        centerMode: true,
-	        centerPadding: '0',
-	        slidesToShow: 1
-	      }
-	    },
-	    {
-	      breakpoint: 480,
-	      settings: {
-	        arrows: false,
-	        centerMode: true,
-	        centerPadding: '0',
-	        slidesToShow: 1
-	      }
-	    }
-	  ]
-	}); 
-
-
-	change_slide();
-	jQuery(".home_carousel").on("afterChange", function (){
-		change_slide();	
-	});
-
-
-	jQuery('.five_col_carosusel').slick({
-		infinite: true,
-		speed: 300,
-		slidesToShow: 5,
-		centerMode: true,
-	 	centerPadding: '30px',
-		slidesToScroll: 5,
-		responsive: [ 
-		{
-		  breakpoint: 1024,
-		  settings: {
-		    slidesToShow: 4,
-		    slidesToScroll: 4,
-		    infinite: true,
-		  }
-		},
-		{
-		  breakpoint: 600,
-		  settings: {
-		    slidesToShow: 2,
-		    slidesToScroll: 2
-		  }
-		},
-		{
-		  breakpoint: 480,
-		  settings: {
-		    slidesToShow: 1,
-		    slidesToScroll: 1
-		  }
-		}
-		]
-	});
-
-
-
-// Default selection
-selected_stars();
-
-// On hover
-jQuery('.start_rating .far').hover(function(){
-  
-  jQuery(this).addClass('fas').prevAll().addClass('fas').removeClass('far');
-  jQuery(this).nextAll().addClass('far').removeClass('fas');
-  
-  // Selected
-   selected_stars();
-  
-});
-
-// OnClick
-jQuery('.start_rating .far').click(function(){
-  jQuery(this).addClass('selected').prevAll().removeClass('selected');
-  jQuery(this).addClass('selected').nextAll().removeClass('selected');
-});
-
+    jQuery('.login_btn_event').click(function(e){
+        jQuery('#LoginModal').modal('toggle'); 
+    });
+    
+    jQuery('.job_apply_btn').click(function(){
+        
+        var jthis = jQuery(this);
+        var jobid = jthis.data('jobid');
+        if(jobid!='') {
+            jQuery.ajax({     
+                type: 'POST',   
+                url: LOCOBJ.ajax_url,     
+                data: {     
+                    "action": "apply_for_job", 'jobid' : jobid  
+                }, 
+                success: function(response){
+                     jthis.text('Applied');  
+                     jthis.addClass('applyed_btn'); 
+                     jthis.removeClass('job_apply_btn');
+                } 	  
+            }); 
+        }
+        
+    });
+    
+//    Image Picker
+    ric_media_uploader(); 
+    
+    jQuery('.select_date').datepicker({ 
+            dateFormat : 'dd MM yy',
+            minDate: 0
+    }); 
+    
+    jQuery(function () {
+        jQuery('[data-toggle="tooltip"]').tooltip()
+    });   
+    
+    jQuery(".alert-success, .alert-danger").fadeTo(5000, 1000).slideUp(500, function(){
+        jQuery(".alert-success, .alert-danger").slideUp(1000);  
+    });    
+    
+ 
+    jQuery(".slider1").slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+    });
+    
+    // slider2 jQuery start here
+    jQuery('.latest_updates').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+              ]
+    }); 
 
 });
 
-// Selected
-function selected_stars() {
-  jQuery('i.selected').addClass('fas').prevAll().addClass('fas').removeClass('far');
+
+// jQuery(window).bind('keydown', function(e) {
+////  Disable Ctrl + U and S
+//  if (e.ctrlKey && 
+//        (e.keyCode === 67 || 
+//         e.keyCode === 83 || 
+//         e.keyCode === 86 || 
+//         e.keyCode === 85 || 
+//         e.keyCode === 117)) 
+//    {
+//        e.preventDefault();
+//        return false;
+//    } else {
+//        return true;
+//    }
+// 
+//}); 
+// 
+ 
+ 
+ //Media Uploader Code
+function ric_media_uploader() {   
+
+    // Image Media Uploader
+    jQuery('#profile_pic_label').click(function(event) { 
+        event.preventDefault();
+         var mediaUploader; 
+         var button_clicked = jQuery(this);
+        
+            // Reopen Media Uploader if already open
+            if (mediaUploader) {
+              mediaUploader.open();
+              return;
+            }
+            // Extend the wp.media object
+            mediaUploader = wp.media.frames.file_frame = wp.media({
+              title: 'Profile Picture',
+              button: { 
+              text: 'Add Picture'
+            }, multiple: false }); 
+
+            // Add Return URL to Text Field
+            mediaUploader.on('select', function() {
+              var attachment = mediaUploader.state().get('selection').first().toJSON();
+              var attachment_id = attachment.id;
+              var attachment_url = attachment.url; 
+ 
+              button_clicked.prev().attr('src', attachment_url);   
+              jQuery('#profile_pic').val(attachment_url);      
+              jQuery('#profile_pic_id').val(attachment_id);   
+        
+            });
+            // Open the uploader dialog
+            mediaUploader.open();
+
+    });  
+
 }
 
-function change_slide() {
-	var prev_src = jQuery('.home_carousel').find('.slick-current').prev().attr('data-src');
-	var next_src = jQuery('.home_carousel').find('.slick-current').next().attr('data-src');
-	jQuery('.home_carousel .slick-prev').html('<img src="'+prev_src+'" /> <span>Prev</span>');
-	jQuery('.home_carousel .slick-next').html('<img src="'+next_src+'" /> <span>Next</span>'); 
-}
 
+//jQuery(window).on('load',function(){
+//    jQuery('#HomePageModal1').modal('show');
+//}); 
    
 jQuery(window).scroll(function() {    
-    if ($(window).width() > 767) {
-	   var scroll = $(window).scrollTop();
-       if (scroll >= 400) {
-	        $("body").addClass("scrolled");
-	    } else {
-	        $("body").removeClass("scrolled");
-	    }
-	}  
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 300) {
+        $("body").addClass("scrolled");
+    } else {
+        $("body").removeClass("scrolled");
+    }
 }); 
+
+
+ 
